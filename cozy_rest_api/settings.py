03d5cd9a360093b9f02c060cc3d6814c249da6ba
Victoria Le <victoriale@Victorias-MacBook-Pro.local>
2022-03-19 22:29:03 -0400

    adding static folder

--- cozy_rest_api/settings.py
+++ cozy_rest_api/settings.py
@@ -131,8 +131,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = 'staticfiles/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+MEDIA_ROOT = 'staticfiles/furnitures-images'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
